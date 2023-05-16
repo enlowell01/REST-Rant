94ebcb1c94c4ba9486027648de6087dc1eac2c55
Evan <evanlowell01@gmail.com>
2023-05-16 15:30:59 -0400

    Adds showing comments functionality

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,27 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">
+                        {c.rant ? 'Rant!' : 'Rave!'}
+                    </h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
@@ -22,7 +43,8 @@ function show (data) {
 
                     </div>
                 </div>
-                <h2>Comments: no comments yet!</h2>
+                <h2>Comments</h2>
+                {comments}
             </main>
         </Def>
     )
