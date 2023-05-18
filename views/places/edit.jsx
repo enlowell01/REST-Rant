536fb66aadbb48bc1f3836f27e005603e12d7fa3
Evan <evanlowell01@gmail.com>
2023-05-18 12:28:28 -0400

    Implement PUT and DELETE

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -5,29 +5,38 @@ function edit_form (data) {
     return (
         <Def>
           <main>
+          <h1>Edit Place</h1>
             <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
-                    <div className="form-group">
+                <div className="row">
+                    <div className="form-group col-sm-6">
                         <label htmlFor="name">Place Name</label>
-                        <input className="form-control" id="name" name="name" value={data.place.name} required/>
+                        <input className="form-control" id="name" name="name" value={data.place.name} required />
                     </div>
-                    <div className="form-group">
+                    <div className="form-group col-sm-6">
                         <label htmlFor="pic">Place Picture</label>
-                        <input className="form-control"  type="url" id="pic" name="pic" value={data.place.pic}/>
+                        <input className="form-control" id="pic" name="pic" value={data.place.pic} />
                     </div>
-                    <div className="form-group">
+                </div>
+                <div className="row">
+                    <div className="form-group col-sm-4">
                         <label htmlFor="city">City</label>
-                        <input className="form-control"  type="url" id="city" name="city" value={data.place.city}/>
+                        <input className="form-control" id="city" name="city" value={data.place.city} />
                     </div>
-                    <div className="form-group">
+                    <div className="form-group col-sm-4">
                         <label htmlFor="state">State</label>
-                        <input className="form-control"  type="url" id="state" name="state" value={data.place.state}/>
+                        <input className="form-control" id="state" name="state" value={data.place.state} />
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="cuisines">Cuisines</label>
-                        <input className="form-control"  type="url" id="cuisines" name="cuisines" value={data.place.cuisines} required />
+                    <div className="form-group col-sm-4">
+                        <label htmlFor="founded">Founded</label>
+                        <input className="form-control" id="founded" name="founded" value={data.place.founded} />
                     </div>
-                    <input className="btn btn-primary" type="submit" value="Add Place" />
-                </form>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="cuisines">Cuisines</label>
+                    <input className="form-control" id="cuisines" name="cuisines" value={data.place.cuisines} required />
+                </div>
+                <input className="btn btn-primary" type="submit" value="Update Place" />
+            </form>
           </main>
         </Def>
     )
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -17,16 +17,20 @@ function show (data) {
             let sumRatings = data.place.comments.reduce((tot, c) => {
                 return tot + c.stars
             }, 0)
-            let averageRating = sumRatings / data.place.comments.length
+            let averageRating = Math.round(sumRatings / data.place.comments.length)
+            let stars = ''
+            for (let i = 0; i < averageRating; i++) {
+                stars += '⭐️'
+            }
             rating = (
                 <h3>
-                    {Math.round(averageRating)} stars
+                    {stars} stars
                 </h3>
             )
             return (
                 <div className="border">
                     <h2 className="rant">
-                        {c.rant ? 'Rant!' : 'Rave!'}
+                        {c.rant ? 'Rant! 😡' : 'Rave! 😻'}
                     </h2>
                     <h4>{c.content}</h4>
                     <h3>
